8af03118864e61ca35397415af8ca2632d6505c0
Xzonn <Xzonn@outlook.com>
2024-04-24 19:36:46 +0800

    Update README

---
+++ README.md
@@ -0,0 +1,31 @@
+# 《悠悠一夏十五载 谋杀谜案悖论》机翻汉化
+
+[![CC-BY-NC-SA 4.0](https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-nc-sa.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)
+
+## 基本说明
+**注意：本补丁仅用作技术交流，请用于正版游戏。**
+
+本项目是对《悠悠一夏十五载 谋杀谜案悖论》（<span lang="ja">マーダーミステリーパラドクスこのひと夏の十五年</span>）的机翻汉化。
+
+本补丁主要采用 [SakuraLLM](https://github.com/SakuraLLM/Sakura-13B-Galgame) 进行翻译，部分文本采用[百度翻译](https://fanyi.baidu.com/)和[有道翻译](https://fanyi.youdao.com/)进行补充。
+
+**由于机翻模型的局限性，部分文本可能存在词不达意、人称混乱、控制符错位、标点符号错误等问题。尽管在机翻前已经对文本进行了预处理，但并不能解决所有问题。由于 Xzonn 的时间和精力有限，无法对所有文本进行人工校对。因此，如果您发现了任何问题，欢迎参与汉化工作。**
+
+## 使用方式
+（TODO）
+
+## 截图预览
+（TODO）
+
+## 相关链接
+- **[>>> 补丁下载地址 <<<](https://github.com/Qi-Busiyi-Hanhuazu/MurderMysteryParadoxChsLocalization/releases/latest/)**
+- [预览视频](https://www.bilibili.com/video/BV1rW421w7B4/)
+
+## 授权协议
+本项目使用 **[CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)** 协议授权。若您想基于本项目进行二次创作，请遵守协议内容。这些内容主要包括：
+
+- **署名**：您必须标明本项目的作者，并在您的作品中提供指向本项目的链接。
+- **非商业性使用**：您不得将本项目用于商业目的。
+- **相同方式共享**：若您基于本项目进行二次创作，您必须以相同的协议授权您的作品。
+
+请阅读本项目的 **[完整授权协议](LICENSE)** 以了解更多信息。
--- scripts/helper.py
+++ scripts/helper.py
@@ -8,6 +8,7 @@ DIR_OUTPUT = os.environ.get("MMP_DIR_OUTPUT", "./out")
 DIR_METADATA = os.environ.get("MMP_DIR_METADATA", "./metadata")
 LANGUAGE = os.environ.get("MMP_LANGUAGE", "zh_Hans")
 
+
 def get_sheet(csv_path: str) -> dict[str, str] | None:
   if not os.path.exists(csv_path):
     return None
@@ -23,8 +24,9 @@ def get_sheet(csv_path: str) -> dict[str, str] | None:
     key = item_dict["id"]
     content = item_dict["target"].replace("\n", "[r]")
     sheet_data[key] = content
-  
+
   return sheet_data
+
 
 def replace_speakers(translation: dict[str, str], text: str) -> str:
   for key in translation.keys():
@@ -32,6 +34,8 @@ def replace_speakers(translation: dict[str, str], text: str) -> str:
       continue
     speaker = key.split("_")[-1]
     text = re.sub(rf"(^#){speaker}$", rf"\1{translation[key]}", text, 0, re.MULTILINE)
-    text = re.sub(rf"(\[(?:chara_display|chara_mod|chara_change|change_scene)(?: (?:time|bg)=['\"].+?['\"])? (?:name|chara)=['\"]){speaker}(?=['\"])", rf"\1{translation[key]}", text, 0, re.MULTILINE)
+    text = re.sub(
+        rf"(\[(?:chara_display|chara_mod|chara_change|change_scene)(?: (?:time|bg)=['\"].+?['\"])? (?:name|chara)=['\"]){speaker}(?=['\"])",
+        rf"\1{translation[key]}", text, 0, re.MULTILINE)
 
   return text
